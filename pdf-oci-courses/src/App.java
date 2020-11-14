a7be94e0f0db3250d6125773271e94bedce4ba61
absalon <luisopazo@gmail.com>
2020-11-14 14:08:39 -0300

    test

--- pdf-oci-courses/src/App.java
+++ pdf-oci-courses/src/App.java
@@ -7,13 +7,13 @@ import com.itextpdf.io.image.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String name = "Administering-Exadata-Cloud@Customer-Gen-2";
+        String name = "autonomous-specialty";
         PdfDocument pdf = new PdfDocument(new PdfWriter("/home/absalon/"+name+".pdf"));
         Document document = new Document(pdf);
         //String line = "Hello! Welcome to iTextPdf";
         //document.add(new Paragraph(line));
 
-        for(int i=1; i < 502 ; i++){
+        for(int i=1; i < 524 ; i++){
             ImageData imageData = ImageDataFactory.create("/home/absalon/oci-pdfs/"+i+".jpg");
             Image pdfImg = new Image(imageData);
         
