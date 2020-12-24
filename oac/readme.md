f1bab9bbece19cbb0fabc685fff24e7ff2d67307
absalon <luisopazo@gmail.com>
2020-12-24 10:57:20 -0300

    test

--- oac/readme.md
+++ oac/readme.md
@@ -4,7 +4,8 @@ ssh -i ssh-key-2020-12-23.key opc@132.145.185.243
 # scp
 scp -i ssh-key-2020-12-23.key ssh-key-2020-12-23.key opc@132.145.185.243:/home/opc
 
- # ProxyJump
+# ProxyJump
+
 ---
 
 # VM privada
--- terraform/oci/provider-vars.tf
+++ terraform/oci/provider-vars.tf
@@ -32,3 +32,26 @@ variable "ssh_public_key" {
 variable "ssh_private_key" {
     default ="oci_api_key.pem"
 }
+
+# ---- 
+
+variable "instance_image_ocid" {
+  type = map(string)
+
+  default = {
+    # See https://docs.us-phoenix-1.oraclecloud.com/images/
+    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
+    us-phoenix-1   = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaablheqkh4k2mo4l5wfnpg2t5zuokmgai5cex6kell4epiio5yi6lq"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaaf2wxqc6ee5axabpbandk6ji27oyxyicatqw5iwkrk76kecqrrdyq"
+    
+  }
+}
+
+variable "instance_shape" {
+  default = "VM.Standard2.1"
+}
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
+}
