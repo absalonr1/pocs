5e9b374ba6567337e5b56eecf925e449e1b8ac09
absalon <luisopazo@gmail.com>
2021-01-01 19:55:22 -0300

    test

--- terraform/oci/rdg/global-vars.tf
+++ terraform/oci/rdg/global-vars.tf
@@ -6,24 +6,28 @@ variable "compartment_ocid" {}
 variable "region" {}
 variable "vm_ssh_public_key"{}
 
-variable "instance_image_ocid" {
-  type = map(string)
 
-  default = {
-    # See https://docs.us-phoenix-1.oraclecloud.com/images/
-    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
-    us-phoenix-1   = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaablheqkh4k2mo4l5wfnpg2t5zuokmgai5cex6kell4epiio5yi6lq"
-    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaaf2wxqc6ee5axabpbandk6ji27oyxyicatqw5iwkrk76kecqrrdyq"
-    
-  }
-}
 variable "instance_image_ocid_win" {
   type = map(string)
 
   default = {
-    # See https://docs.us-phoenix-1.oraclecloud.com/images/
-    # Windows-Server-2019-Standard-Edition-VM-Gen2-2020.10.22-0
+    # https://docs.oracle.com/en-us/iaas/images/image/88496145-3cf5-4b40-a466-70a290499a0b/
+    # Windows-Server-2016-Standard-Edition-VM
     us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaayxmorhjrkcraeoeaid2ijneuwtupddliy3oe7yv7kbjwinnop3rq"
+    sa-santiago-1  = "ocid1.image.oc1.sa-santiago-1.aaaaaaaa6ofv25fel7cmic4y6d4rjpjgo3giymxl5brlqhm2efmuw4oytxhq"
+    
+  }
+}
+
+variable "instance_image_ocid" {
+  type = map(string)
+
+  default = {
+    # https://docs.oracle.com/en-us/iaas/images/image/c7b501b9-f2fa-40e9-b416-299cafdad0e2/
+    # 	Oracle-Linux-7.9-2020.11.10-1
+    us-phoenix-1   = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaablheqkh4k2mo4l5wfnpg2t5zuokmgai5cex6kell4epiio5yi6lq"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaaf2wxqc6ee5axabpbandk6ji27oyxyicatqw5iwkrk76kecqrrdyq"
+    sa-santiago-1  = "ocid1.image.oc1.sa-santiago-1.aaaaaaaayczdfud4bdncfbnq7agblbshdcswkxsd7qcab3qy3nbjl7ev7edq"
     
   }
 }
@@ -33,19 +37,17 @@ variable "instance_image_ocid_linux_rdg_custom" {
   type = map(string)
 
   default = {
-    # See https://docs.us-phoenix-1.oraclecloud.com/images/
-    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0" 
+    # https://docs.oracle.com/en-us/iaas/images/image/c7b501b9-f2fa-40e9-b416-299cafdad0e2/
+    # BASADA EN: 	Oracle-Linux-7.9-2020.11.10-1
     # Incluye instalacion de RDG 5.8
     us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaaraizp7oxtm47pk25gicdwjiwj7ar6s2eef5xcgxgkippu5rj7xsq"
-    
+    sa-santiago-1  = ""
+
   }
 }
 variable "instance_shape" {
   default = "VM.Standard2.1"
 }
-
-
-
 
 variable "db_system_shape"{
   default="VM.Standard2.1"
